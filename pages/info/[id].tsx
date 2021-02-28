35756c694d783b2ba43e5b5548f2b851fcd1e9f2
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-02-28 19:59:56 +0530

    Added UI icons and list cards

--- pages/info/[id].tsx
+++ pages/info/[id].tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import nutridigmServerInstance from "../../config/api/nutridigmServerInstance";
 import { useRouter } from "next/router";
 import ListCard from "../../components/ListCard";
-import { FcCheckmark } from "react-icons/fc";
+import { FcCheckmark, FcApproval, FcHighPriority } from "react-icons/fc";
 import { HiOutlineX } from "react-icons/hi";
 
 export default function DiseaseInfo() {
@@ -29,31 +29,45 @@ export default function DiseaseInfo() {
     console.log(avoidItems);
 
     return (
-        <div className='flex flex-col'>
-            <h3>{title}</h3>
-            <div className='flex flex-row'>
-                {avoidItems &&
-                    avoidItems
-                        .split("; ")
-                        .map((item) => (
-                            <ListCard
-                                title={item}
-                                icon={<FcCheckmark />}
-                                iconClasses='bg-gray-800'
-                            />
-                        ))}
-            </div>
-            <div className="flex flex-row">
-                {consumeItems &&
-                    consumeItems
-                        .split("; ")
-                        .map((item) => (
-                            <ListCard
-                                title={item}
-                                icon={<HiOutlineX />}
-                                iconClasses='bg-gray-800'
-                            />
-                        ))}
+        <div className='flex flex-col items-center'>
+            <h3 className="text-7xl font-bold my-2 text-gray-700">{title}</h3>
+
+            <hr className="my-6 w-full border-2 border-gray-200" />
+
+            <div className="flex flex-col items-center">
+                <h3 className="flex flex-row items-center text-4xl font-light my-6 text-gray-700">
+                    Food items that will make your body happy&nbsp;<FcApproval />
+                </h3>
+                <div className='flex flex-row flex-wrap justify-center'>
+                    {avoidItems &&
+                        avoidItems
+                            .split("; ")
+                            .map((item) => (
+                                <ListCard
+                                    title={item}
+                                    icon={<FcCheckmark />}
+                                    iconClasses='bg-gray-800'
+                                />
+                            ))}
+                </div>
+
+                <hr className="my-6 w-full border-2 border-gray-200" />
+
+                <h3 className="flex flex-row items-center text-4xl font-light my-6 text-gray-700">
+                    Food items that will make you regret&nbsp;<FcHighPriority />
+                </h3>
+                <div className="flex flex-row flex-wrap justify-center">
+                    {consumeItems &&
+                        consumeItems
+                            .split("; ")
+                            .map((item) => (
+                                <ListCard
+                                    title={item}
+                                    icon={<HiOutlineX />}
+                                    iconClasses='bg-gray-800'
+                                />
+                            ))}
+                </div>
             </div>
         </div>
     );
