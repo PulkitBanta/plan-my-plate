45e42da71f040426082f75e1f02e7efa4802cbf1
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-02-28 13:01:46 +0530

    Moved data to faux api

---
+++ pages/api/diseases.ts
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function helloAPI(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    data: [
+      {
+        title: "Plan My Plate",
+        description:
+          "Laborum et aute cillum mollit. Magna quis quis incididunt deserunt. Do occaecat et tempor nulla consectetur officia mollit Lorem exercitation.",
+        diseases: [
+          {
+            name: "Covid-19",
+            description: "Mollit officia culpa officia mollit dolor cillum.",
+            protein: "59",
+            nutrients: "46",
+          },
+          {
+            name: "Disease 2",
+            description: "Mollit officia culpa officia mollit dolor cillum.",
+            protein: "85",
+            nutrients: "35",
+          },
+          {
+            name: "Disease 3",
+            description: "Mollit officia culpa officia mollit dolor cillum.",
+            protein: "65",
+            nutrients: "21",
+          },
+          {
+            name: "Disease 4",
+            description: "Mollit officia culpa officia mollit dolor cillum.",
+            protein: "70",
+            nutrients: "64",
+          },
+          {
+            name: "Disease 5",
+            description: "Mollit officia culpa officia mollit dolor cillum.",
+            protein: "35",
+            nutrients: "41",
+          },
+        ],
+      },
+    ],
+  });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Card from '../components/Card';
 
 export default function Home(): JSX.Element {
   const arr = ['hello', 'bro', 'whats', 'up'];
+  fetch('http://localhost:3000/api/hello')
+    .then(response => response.json())
+    .then(data => console.log(data));
   return (
     <div className="h-screen w-screen bg-gray-300 p-6">
       {/* Header */}
